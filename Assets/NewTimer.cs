d80ad48d523dd1dcad49002546a17d171e3bf3b0
c0nques7 <c0nques7@gmail.com>
2018-11-06 16:55:21 -0500

    Sped Up Animations
    Sped up the end game and new high score animations.

--- Assets/NewTimer.cs
+++ Assets/NewTimer.cs
@@ -14,6 +14,10 @@ public class NewTimer : MonoBehaviour {
     public Text highScore;
     public bool pauseTimer = true;
     public bool resetTriggered;
+    
+    //Only enable this if you move something up on the HUDCanvas and stuff stops working
+    //Jump to line 66
+    //bool hudChecked;
     bool hScore;
 
     bool playSound1;
@@ -58,13 +62,15 @@ public class NewTimer : MonoBehaviour {
 
     public void Awake()
      {
-        hScore = false;
+        //After this is enabled, jump to line 90
+        //hudChecked = false;
         playedSound = false;
         playSound1 = false;
         playSound2 = false;
         playSound3 = false;
         playSound4 = false;
         gameStarted = false;
+        hScore = false;
         hudAnim = hudCanvas.GetComponent<Animator>();
 		anim = GetComponentInChildren<Animator>();
 		m_Renderer = GetComponent<MeshRenderer>();
@@ -79,8 +85,15 @@ public class NewTimer : MonoBehaviour {
 
         hudText = GameObject.Find("HUDObject").GetComponentsInChildren<Text>();
         pointCounter = GameObject.Find("HUDObject").GetComponentInChildren<PointCounter>();
-  
-        if (hudText[9].color.a >= 0.95 && playedSound == false && playSound1 == false)
+        
+        //Finally, enable lines 154-326
+        /*if(hudText != null && hudChecked == false)
+        {
+            CheckForHUDObject();
+            hudChecked = true;
+        }*/
+
+        if (hudText[12].color.a >= 0.9 && playedSound == false && playSound1 == false)
         {
             playSound1 = true;
             if (playSound1 == true)
@@ -90,7 +103,7 @@ public class NewTimer : MonoBehaviour {
             }
             
         }
-        if (hudText[10].color.a >= 0.95 && playSound2 == false && playedSound == false)
+        if (hudText[13].color.a >= 0.9 && playSound2 == false && playedSound == false)
         {
             playSound2 = true;
             playedSound = true;
@@ -100,7 +113,7 @@ public class NewTimer : MonoBehaviour {
                 playedSound = false;
             }
         }
-        if (hudText[11].color.a >= 0.95 && playSound3 == false && playedSound == false)
+        if (hudText[14].color.a >= 0.9 && playSound3 == false && playedSound == false)
         {
             playSound3 = true;
             playedSound = true;
@@ -110,7 +123,7 @@ public class NewTimer : MonoBehaviour {
                 playedSound = false;
             }
         }
-        if (hudText[12].color.a >= 0.95 && playSound4 == false && playedSound == false)
+        if (hudText[15].color.a >= 0.9 && playSound4 == false && playedSound == false)
         {
             playSound4 = true;
             playedSound = true;
@@ -136,6 +149,180 @@ public class NewTimer : MonoBehaviour {
 
 
      }
+    //Only enable this if you move something in the HUDCanvas
+     /*void CheckForHUDObject()
+     {
+         if(hudText[0] != null)
+        {
+            Debug.Log("0 here! I'm present, and my name is: " + hudText[0]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 0 is not present");
+        }
+
+        if(hudText[1] != null)
+        {
+            Debug.Log("1 here! I'm present, and my name is: " + hudText[1]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 1 is not present");
+        }
+
+        if(hudText[2] != null)
+        {
+            Debug.Log("2 here! I'm present, and my name is: " + hudText[2]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 2 is not present");
+        }
+
+        if(hudText[3] != null)
+        {
+            Debug.Log("3 here! I'm present, and my name is: " + hudText[3]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 3 is not present");
+        }
+
+        if(hudText[4] != null)
+        {
+            Debug.Log("4 here! I'm present, and my name is: " + hudText[4]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 4 is not present");
+        }
+
+        if(hudText[5] != null)
+        {
+            Debug.Log("5 here! I'm present, and my name is: " + hudText[5]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 5 is not present");
+        }
+
+        if(hudText[6] != null)
+        {
+            Debug.Log("6 here! I'm present, and my name is: " + hudText[6]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 6 is not present");
+        }
+
+        if(hudText[7] != null)
+        {
+            Debug.Log("7 here! I'm present, and my name is: " + hudText[7]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 7 is not present");
+        }
+
+        if(hudText[8] != null)
+        {
+            Debug.Log("8 here! I'm present, and my name is: " + hudText[8]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 8 is not present");
+        }
+
+        if(hudText[9] != null)
+        {
+            Debug.Log("9 here! I'm present, and my name is: " + hudText[9]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 9 is not present");
+        }
+
+        if(hudText[10] != null)
+        {
+            Debug.Log("10 here! I'm present, and my name is: " + hudText[10]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 10 is not present");
+        }
+
+        if(hudText[11] != null)
+        {
+            Debug.Log("11 here! I'm present, and my name is: " + hudText[11]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 11 is not present");
+        }
+
+        if(hudText[12] != null)
+        {
+            Debug.Log("12 here! I'm present, and my name is: " + hudText[12]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 12 is not present");
+        }
+
+        if(hudText[13] != null)
+        {
+            Debug.Log("13 here! I'm present, and my name is: " + hudText[13]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 13 is not present");
+        }
+
+        if(hudText[14] != null)
+        {
+            Debug.Log("14 here! I'm present, and my name is: " + hudText[14]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 14 is not present");
+        }
+        
+        if(hudText[15] != null)
+        {
+            Debug.Log("15 here! I'm present, and my name is: " + hudText[15]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 15 is not present");
+        }
+        
+        if(hudText[16] != null)
+        {
+            Debug.Log("16 here! I'm present, and my name is: " + hudText[16]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 16 is not present");
+        }
+        
+        if(hudText[17] != null)
+        {
+            Debug.Log("17 here! I'm present, and my name is: " + hudText[17]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 17 is not present");
+        }
+        
+        if(hudText[18] != null)
+        {
+            Debug.Log("18 here! I'm present, and my name is: " + hudText[18]);
+        }
+        else
+        {
+            Debug.Log("HUD Text Object 18 is not present");
+        }
+     }*/
 
 	 void OnMouseOver()
 	 {
@@ -194,6 +381,11 @@ public class NewTimer : MonoBehaviour {
          //Debug.Log("The best time has been reset");
      }
 
+     void ResetHighScore()
+     {
+         PlayerPrefs.DeleteKey("BestTime");
+     }
+
     //Stop Timer
     public void StopTimer()
     {
@@ -208,22 +400,17 @@ public class NewTimer : MonoBehaviour {
             pointCounter.highScore.text = PlayerPrefs.GetFloat("BestTime").ToString("Fastest Time: " + "0:00.00");
             Debug.Log("The best time is: " + PlayerPrefs.GetFloat("BestTime"));
         }
-        
-            
+
         if(hScore == true)
         {
             hudAnim.SetBool("HighScore", true);
             hScore = false;
         }
-              
+
         Debug.Log("Timer Stopped");
         //Stop Timer Here
         anim.SetBool("TimerStarted", false);
         pauseTimer = false;
-        
-        
-        
-
     }
  
      //Start Timer
