2271b3b283ce686a7aacf3dcbcdbbe9498820865
c0nques7 <c0nques7@gmail.com>
2018-11-07 17:03:31 -0500

    Began editing animations for sci-fi weapons
    Today I started the process of pulling and modifying the running, shooting and idle animations for our sci-fi weapons. It's not working properly right now, but should be adressed in next push.

---
+++ Assets/TestShooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestShooting : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) == true)
+        {
+            anim.SetTrigger("Shoot");
+        }
+	}
+}
