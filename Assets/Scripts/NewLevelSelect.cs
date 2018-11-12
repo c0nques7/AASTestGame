b0b4b65e63c3dfc6533f34d3c40ed704ad1cba2a
c0nques7 <c0nques7@gmail.com>
2018-11-12 17:02:33 -0500

    Core Functionality Done
    Big update. Core functionality is what I'm calling "done". Nothing new needs to be built out at this point, and now the fine tuing begins. Game can be launched from TestMenu.

---
+++ Assets/Scripts/NewLevelSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLevelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadFirstGallery()
+    {
+        SceneManager.LoadScene("FirstGallery");
+    }
+    public void LoadFirstArena()
+    {
+        SceneManager.LoadScene("FirstArena");
+    }
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("TestMenu");
+    }
+    
+}
